5875fba2e908c062d9d610fdffefb33b848e2e3a
ngoviet <ngoducvietds@gmail.com>
2015-01-07 11:02:22 -0800

    upload 3

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravelapp1',
+			'database'  => 'laravelapp',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
