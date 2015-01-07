db5ccaf6ee519dabfa32d804e206773138edcbdf
ngoviet <ngoducvietds@gmail.com>
2015-01-07 10:58:09 -0800

    upload 2

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravelapp',
+			'database'  => 'laravelapp1',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
